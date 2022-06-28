69fc12079f6555bb650f138a5aee874eeb083251
Tyler Ryden <tryden11@gmail.com>
2022-06-27 20:32:03 -0500

    recreated PayoutsDetail activity

--- app/src/main/java/com/veryable/android/view/PayoutsDetailActivity.kt
+++ app/src/main/java/com/veryable/android/view/PayoutsDetailActivity.kt
@@ -2,11 +2,17 @@ package com.veryable.android.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.veryable.android.R
+import com.veryable.android.databinding.ActivityPayoutsDetailBinding
+import com.veryable.android.databinding.ActivityPayoutsListBinding
 
 class PayoutsDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPayoutsDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_payouts_detail)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_payouts_detail)
     }
 }
