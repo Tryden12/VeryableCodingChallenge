c7a1eeeddf93013fe4a210fe22321feec7da61f7
Tyler Ryden <tryden11@gmail.com>
2022-06-29 13:59:28 -0500

    adapter progress

--- app/src/main/java/com/veryable/android/adapter/Adapter.kt
+++ app/src/main/java/com/veryable/android/adapter/Adapter.kt
@@ -3,23 +3,27 @@ package com.veryable.android.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.veryable.android.R
 import com.veryable.android.data.Account
+import kotlinx.android.synthetic.main.list_item.view.*
 
 class Adapter : RecyclerView.Adapter<Adapter.ItemViewHolder>() {
 
-    private var list: List<Account>? = null
+    private var accountList: List<Account>? = null
 
-    fun setList(list: List<Account>?) {
-        this.list = list
+    fun setAccountList(accountList: List<Account>?) {
+        this.accountList = accountList
     }
 
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.list_item, parent, false) as TextView
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): Adapter.ItemViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
         return ItemViewHolder(view)
     }
 
@@ -28,14 +32,28 @@ class Adapter : RecyclerView.Adapter<Adapter.ItemViewHolder>() {
     }
 
     override fun getItemCount(): Int {
-        return list?.size ?: 0
+        if (accountList == null) return 0
+        else return accountList?.size!!
     }
 
 
     /************ Item View Holder Inner Class ******************/
-    inner class ItemViewHolder(val view: TextView) :
+    inner class ItemViewHolder(val view: View) :
         RecyclerView.ViewHolder(view),
         View.OnClickListener {
+
+        val accountImage: ImageView    = view.item_icon
+        val itemTitle: TextView        = view.item_title_textview
+        val itemDesc: TextView         = view.item_desc_textview
+        val itemDescDelivery: TextView = view.item_desc_delivery_textview
+
+        fun bind(data : Account?) {
+            if (data != null) {
+                itemTitle.text = data.accountName ?: ""
+                itemDesc.text = data.description ?: ""
+            }
+        }
+
 
         init {
             view.setOnClickListener(this)
@@ -43,10 +61,6 @@ class Adapter : RecyclerView.Adapter<Adapter.ItemViewHolder>() {
 
         override fun onClick(v: View?) {
             TODO("Not yet implemented")
-        }
-
-        fun bind(account : Account?) {
-
         }
 
     }
