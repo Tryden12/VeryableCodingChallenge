c29861af19d210c7f25e2a6288b3b96539d256b7
Tyler Ryden <tryden11@gmail.com>
2022-06-29 10:15:24 -0500

    started adapter

--- app/src/main/java/com/veryable/android/adapter/Adapter.kt
+++ app/src/main/java/com/veryable/android/adapter/Adapter.kt
@@ -1,4 +1,53 @@
 package com.veryable.android.adapter
 
-class Adapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.veryable.android.R
+import com.veryable.android.data.Account
+
+class Adapter : RecyclerView.Adapter<Adapter.ItemViewHolder>() {
+
+    private var list: List<Account>? = null
+
+    fun setList(list: List<Account>?) {
+        this.list = list
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false) as TextView
+        return ItemViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        return list?.size ?: 0
+    }
+
+
+    /************ Item View Holder Inner Class ******************/
+    inner class ItemViewHolder(val view: TextView) :
+        RecyclerView.ViewHolder(view),
+        View.OnClickListener {
+
+        init {
+            view.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            TODO("Not yet implemented")
+        }
+
+        fun bind(account : Account?) {
+
+        }
+
+    }
 }
--- app/src/main/java/com/veryable/android/view/PayoutsListActivity.kt
+++ app/src/main/java/com/veryable/android/view/PayoutsListActivity.kt
@@ -3,15 +3,27 @@ package com.veryable.android.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.veryable.android.R
+import com.veryable.android.adapter.Adapter
 import com.veryable.android.databinding.ActivityPayoutsListBinding
 
 class PayoutsListActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityPayoutsListBinding
+    private lateinit var bankAdapter: Adapter
+    private lateinit var cardAdapter: Adapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_payouts_list)
+    }
+
+    private fun initRecyclerView(){
+        binding.bankListRecyclerview.apply {
+            layoutManager = LinearLayoutManager(this@PayoutsListActivity)
+            bankAdapter = Adapter()
+            adapter = bankAdapter
+        }
     }
 }
