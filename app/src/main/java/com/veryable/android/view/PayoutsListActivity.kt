59bdbd7a98a04265e52f423ada14453d2aa56a6c
Tyler Ryden <tryden11@gmail.com>
2022-06-29 15:30:15 -0500

    progress in applying adapter to recyclerviews

--- app/src/main/java/com/veryable/android/view/PayoutsListActivity.kt
+++ app/src/main/java/com/veryable/android/view/PayoutsListActivity.kt
@@ -2,11 +2,16 @@ package com.veryable.android.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.veryable.android.R
 import com.veryable.android.adapter.Adapter
 import com.veryable.android.databinding.ActivityPayoutsListBinding
+import com.veryable.android.viewmodel.PayoutsListViewModel
 
 class PayoutsListActivity : AppCompatActivity() {
 
@@ -17,6 +22,14 @@ class PayoutsListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_payouts_list)
+
+        initView()
+        initRecyclerView()
+        initViewModel()
+    }
+
+    private fun initView() {
+        setSupportActionBar(binding.toolbar)
     }
 
     private fun initRecyclerView(){
@@ -26,4 +39,19 @@ class PayoutsListActivity : AppCompatActivity() {
             adapter = bankAdapter
         }
     }
-}
+
+    private fun initViewModel() {
+        val viewModel: PayoutsListViewModel = PayoutsListViewModel()
+        viewModel.getLiveDataObserver().observe(this@PayoutsListActivity, Observer {
+            if (it != null) {
+                bankAdapter.setAccountList(it)
+                bankAdapter.notifyDataSetChanged()
+            } else {
+                Toast.makeText(this, "Error retrieving data", Toast.LENGTH_SHORT).show()
+            }
+        })
+        //viewModel.makeApiCall()
+    }
+
+    }
+
