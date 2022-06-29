10d9a6c4b6f3df29820bf0d6b2a2d233d45bbfe7
Tyler Ryden <tryden11@gmail.com>
2022-06-28 21:23:08 -0500

    Moshi dependencies added and RetroInstance is finished

---
+++ app/src/main/java/com/veryable/android/retrofit/RetroInstance.kt
@@ -0,0 +1,38 @@
+package com.veryable.android.retrofit
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+object RetroInstance {
+
+    // Base Url
+    private const val BASE_URL: String =
+        "https://veryable-public-assets.s3.us-east-2.amazonaws.com/veryable.json"
+
+    // Create Moshi instance
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()!!
+
+    // Http Client
+    private val httpClient: OkHttpClient by lazy {
+        OkHttpClient.Builder().build()
+    }
+
+    // Create Retro instance
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(httpClient)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+    }
+
+    // Create Api Service
+    val apiService: ApiService by lazy {
+        retrofit.create(ApiService::class.java)
+    }
+}
