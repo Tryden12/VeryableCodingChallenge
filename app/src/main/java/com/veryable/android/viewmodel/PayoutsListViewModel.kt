db494b5318c7462d348ca8bef51023cbe79590dc
Tyler Ryden <tryden11@gmail.com>
2022-06-29 09:16:39 -0500

    viewmodel ready

--- app/src/main/java/com/veryable/android/viewmodel/PayoutsListViewModel.kt
+++ app/src/main/java/com/veryable/android/viewmodel/PayoutsListViewModel.kt
@@ -1,7 +1,45 @@
 package com.veryable.android.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.veryable.android.data.Account
+import com.veryable.android.retrofit.RetroInstance.apiService
+import retrofit2.Call
+import retrofit2.Response
 
 class PayoutsListViewModel : ViewModel() {
 
+    // Live data for the list from api
+    var liveDataList: MutableLiveData<MutableList<Account>> = MutableLiveData()
+
+    // Live data for each item
+    var liveItemData: MutableLiveData<Account> = MutableLiveData()
+
+    // Observer for live list
+    fun getLiveDataObserver(): MutableLiveData<MutableList<Account>> {
+        return liveDataList
+    }
+
+    // Observer for each item
+    fun getLiveItemObserver(): MutableLiveData<Account> {
+        return liveItemData
+    }
+
+    // Api call for list data
+    suspend fun makeApiCall() {
+        val call = apiService.getData()
+        call.enqueue(object : retrofit2.Callback<MutableList<Account>> {
+            override fun onResponse(
+                call: Call<MutableList<Account>>,
+                response: Response<MutableList<Account>>,
+            ) {
+                liveDataList.postValue(response.body())
+            }
+
+            override fun onFailure(call: Call<MutableList<Account>>, t: Throwable) {
+                liveDataList.postValue(null)
+            }
+
+        })
+    }
 }
