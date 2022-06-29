f46e2375d775d1e7ab6d00c6a251062edbc39df4
Tyler Ryden <tryden11@gmail.com>
2022-06-28 21:35:26 -0500

    Account model & Api interface complete

--- app/src/main/java/com/veryable/android/data/Account.kt
+++ app/src/main/java/com/veryable/android/data/Account.kt
@@ -1,4 +1,30 @@
 package com.veryable.android.data
 
-class Account {
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class Account (
+
+    @Expose
+    @SerializedName("id")
+    val id : String? = null,
+
+    @Expose
+    @SerializedName("account_type")
+    val accountType : String? = null,
+
+    @Expose
+    @SerializedName("account_name")
+    val accountName : String? = null,
+
+    @Expose
+    @SerializedName("desc")
+    val description : String? = null
+) {
+    override fun toString(): String {
+        return "id = $id, " +
+                "account type = $accountType, " +
+                "account name = $accountName, " +
+                "description = $description"
+    }
 }
--- app/src/main/java/com/veryable/android/retrofit/ApiService.kt
+++ app/src/main/java/com/veryable/android/retrofit/ApiService.kt
@@ -1,4 +1,12 @@
 package com.veryable.android.retrofit
 
+import android.telecom.Call
+import com.veryable.android.data.Account
+import retrofit2.http.GET
+
 interface ApiService {
+
+    @GET("veryable.json")
+    suspend fun getData(): retrofit2.Call<MutableList<Account>>
+
 }
