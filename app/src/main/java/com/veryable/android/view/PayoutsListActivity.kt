12547d4725313c9e9be92872fc989be486ade71c
Tyler Ryden <tryden11@gmail.com>
2022-06-30 09:26:38 -0500

    added divider in recyclerview

--- app/src/main/java/com/veryable/android/view/PayoutsListActivity.kt
+++ app/src/main/java/com/veryable/android/view/PayoutsListActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.veryable.android.R
 import com.veryable.android.adapter.Adapter
@@ -34,17 +35,59 @@ class PayoutsListActivity : AppCompatActivity() {
     }
 
     private fun initRecyclerView(){
+        /*
+        val layoutManager = LinearLayoutManager(this)
+        binding.bankListRecyclerview.layoutManager = layoutManager
+
+        // Add divider
+        val divider = DividerItemDecoration(
+            applicationContext, layoutManager.orientation
+        )
+        binding.bankListRecyclerview.addItemDecoration(divider)
+
+        // Bind adapter to recycler view
+        bankAdapter = Adapter()
+        binding.bankListRecyclerview.adapter = bankAdapter
+
+        val layoutManager2 = LinearLayoutManager(this)
+        binding.cardListRecyclerview.layoutManager = layoutManager2
+
+        // Add divider
+        val divider2 = DividerItemDecoration(
+            applicationContext, layoutManager2.orientation
+        )
+        binding.cardListRecyclerview.addItemDecoration(divider)
+
+        // Bind adapter to recycler view
+        cardAdapter = Adapter()
+        binding.cardListRecyclerview.adapter = cardAdapter
+
+       */
         binding.bankListRecyclerview.apply {
             layoutManager = LinearLayoutManager(this@PayoutsListActivity)
             bankAdapter = Adapter()
             adapter = bankAdapter
+
+            // Add divider
+            val divider = DividerItemDecoration(
+                applicationContext, (layoutManager as LinearLayoutManager).orientation
+            )
+            addItemDecoration(divider)
         }
 
         binding.cardListRecyclerview.apply {
             layoutManager = LinearLayoutManager(this@PayoutsListActivity)
             cardAdapter = Adapter()
             adapter = cardAdapter
+
+            // Add divider
+            val divider = DividerItemDecoration(
+                applicationContext, (layoutManager as LinearLayoutManager).orientation
+            )
+            addItemDecoration(divider)
         }
+
+
     }
 
     private fun initViewModel() {
