e23103243831797b930ffdfd80c0fef7cbeb5f84
Tyler Ryden <tryden11@gmail.com>
2022-07-01 10:16:37 -0500

    complete

--- app/src/main/java/com/veryable/android/view/PayoutsDetailActivity.kt
+++ app/src/main/java/com/veryable/android/view/PayoutsDetailActivity.kt
@@ -25,6 +25,7 @@ class PayoutsDetailActivity : AppCompatActivity(),View.OnClickListener {
         binding.doneButton.setOnClickListener(this)
     }
 
+    // Retrieve intent from account item clicked in previous activity
     private fun getIntentData() {
         val intent = intent
         val accountName = intent.getStringExtra("accountName").toString()
@@ -40,6 +41,7 @@ class PayoutsDetailActivity : AppCompatActivity(),View.OnClickListener {
         Log.d("DEBUG_INTENT", "$accountName, $accountDesc")
     }
 
+    // Setup toolbar with back button clickable
     private fun initView() {
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
@@ -49,15 +51,12 @@ class PayoutsDetailActivity : AppCompatActivity(),View.OnClickListener {
         }
     }
 
+    // Go back when done button pressed
     override fun onClick(view: View?) {
         when (view?.id) {
             R.id.done_button -> {
                 onBackPressed()
             }
         }
-    }
-
-    override fun onBackPressed() {
-        super.onBackPressed()
     }
 }
