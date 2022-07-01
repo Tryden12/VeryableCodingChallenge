16ecc4ffda5501d500f810e0a0e74b56aaeab82b
Tyler Ryden <tryden11@gmail.com>
2022-06-30 20:56:57 -0500

    getIntent progress

--- app/src/main/java/com/veryable/android/view/PayoutsDetailActivity.kt
+++ app/src/main/java/com/veryable/android/view/PayoutsDetailActivity.kt
@@ -2,10 +2,12 @@ package com.veryable.android.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import com.veryable.android.R
 import com.veryable.android.databinding.ActivityPayoutsDetailBinding
 import com.veryable.android.databinding.ActivityPayoutsListBinding
+import com.veryable.android.utils.Constants
 
 class PayoutsDetailActivity : AppCompatActivity() {
 
@@ -14,5 +16,14 @@ class PayoutsDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_payouts_detail)
+
+        getIntentData()
+    }
+
+    private fun getIntentData() {
+        val intent = intent
+        val accountName: String = intent.getStringExtra(Constants.ITEM_DATA).toString()
+        val accountNameTextView = findViewById<TextView>(R.id.item_title_textview)
+        accountNameTextView.text = accountName
     }
 }
